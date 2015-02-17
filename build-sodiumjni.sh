9a6c965e6f14a89f80d0e130109866c65c0c3e52
Jack Wink <jackwink@umich.edu>
2015-02-17 11:41:26 -0500

    build script cleanup

--- build-sodiumjni.sh
+++ build-sodiumjni.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-if [ -z "$JAVA_HOME" ]; then
-    echo "ERROR You should set JAVA_HOME"
-    echo "Exiting!"
-    exit 1
-fi
-
-# Root location of the sodium-jni project
-SODIUMJNI_HOME=$(pwd)
-
-# Location of the sodiumJNI java classes (where SWIG should put the generated files)
-SODIUMJNI_SRC_ROOT="${SODIUMJNI_HOME}/app/src"
-
-# libsodium build location
-LIBSODIUM_JNI_HOME="${SODIUMJNI_HOME}/jni"
-
-set -e
-
+##
 # Steps:
 #
 #   1. Build libsodium shared libraries for Android
@@ -26,6 +10,30 @@ set -e
 #   3. Run ndk-build to build our sodiumjni shared libraries
 #
 #   4. Run gradle build script
+
+# Fail if anything returns non-zero
+set -e
+
+require_env_var() {
+    if [ -z "$1" ]; then
+        echo "build-libsodium.sh: ERROR: Required env variable '$2' not set."
+        echo "build-libsodium.sh: Exiting!"
+        exit 1
+    fi
+}
+
+require_env_var "$JAVA_HOME" "JAVA_HOME"
+require_env_var "$ANDROID_HOME" "ANDROID_HOME"
+require_env_var "$ANDROID_NDK_HOME" "ANDROID_NDK_HOME"
+
+# Root location of the sodium-jni project
+SODIUMJNI_HOME=$(pwd)
+
+# Location of the sodiumJNI java classes (where SWIG should put the generated files)
+SODIUMJNI_SRC_ROOT="${SODIUMJNI_HOME}/app/src"
+
+# libsodium build location
+LIBSODIUM_JNI_HOME="${SODIUMJNI_HOME}/jni"
 
 ##
 #   Step 1
@@ -49,4 +57,9 @@ swig -java -package com.jackwink.libsodium -outdir ../app/src/main/java/com/jack
 
 cd $LIBSODIUM_JNI_HOME
 ndk-build
+
+# do some cleanup
+rm -rf $SODIUMJNI_HOME/obj
+rm -rf $SODIUMJNI_SRC_ROOT/main/lib
 mv $SODIUMJNI_HOME/libs $SODIUMJNI_SRC_ROOT/main/lib
+
